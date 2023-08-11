1e8018530c06246bffc363d3cab6fbc4a233d942
Lucas Trubiano <lucas.trubiano@gmail.com>
2023-08-10 23:13:46 -0300

    arreglar cambios

--- facerec_from_webcam_mouth_open.py
+++ facerec_from_webcam_mouth_open.py
@@ -28,65 +28,37 @@ def is_mouth_open(face_landmarks):
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Define the codec and create VideoWriter object to save video to local
-fourcc = cv2.VideoWriter_fourcc(*"XVID")  # codec
-# cv2.VideoWriter( filename, fourcc, fps, frameSize )
-# out = cv2.VideoWriter("output.avi", fourcc, 7, (640, 480))
-
-# Load a sample picture and learn how to recognize it.
-# peter_image = face_recognition.load_image_file(
-#     "peter.jpg"
-# )  # replace peter.jpg with your own image !!
-# peter_face_encoding = face_recognition.face_encodings(peter_image)[0]
-
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Find all the faces and face enqcodings in the frame of video
-    face_locations = [face_recognition.face_locations(frame)[0]]
-    # print("face_locations: ")
-    # print(len(face_locations))
+    face_locations = face_recognition.face_locations(frame)
 
-    # top, right, bottom, left = face_locations[0]
+    if face_locations:
+        top, right, bottom, left = face_locations[0]
 
-    # Obtain a new frame with the face centered
-    # face_image = frame[top:bottom, left:right]
-    
-    face_encodings = face_recognition.face_encodings(
-        frame, face_locations, num_jitters=1, model="small"
-    )
-    # print("face_encodings: ")
-    # print(len(face_encodings))
-    face_landmarks_list = face_recognition.face_landmarks(frame)
-    # print("face_landmarks_list: ")
-    # print(len(face_landmarks_list))
+        # Obtain a new frame with the face centered
+        face_image = frame[top:bottom, left:right]
 
-    # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding, face_landmarks in zip(face_locations, face_encodings, face_landmarks_list):
+        face_landmarks_list = face_recognition.face_landmarks(face_image)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Loop through each face in this frame of video
+        for (top, right, bottom, left), face_landmarks in zip(
+            face_locations, face_landmarks_list
+        ):
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom), (right, bottom + 35), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, "Lucas", (left + 6, bottom + 25), font, 1.0, (255, 255, 255), 1)
+            # Display text for mouth open
+            if is_mouth_open(face_landmarks):
+                # text = 'Mouth is open'
+                print("Mouth is open")
 
-
-        # Display text for mouth open / close
-        ret_mouth_open = is_mouth_open(face_landmarks)
-        if ret_mouth_open is True:
-            text = 'Mouth is open'
-            print('Mouth is open')
-        else:
-            text = 'Open your mouth'
-            print('Open your mouth')
-        cv2.putText(frame, text, (left, top - 50), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+            # cv2.putText(frame, text, (left, top - 50), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow("Video", frame)
-    # out.write(frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord("q"):
